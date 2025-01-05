d52059bfd8eeb4f928d33b6edd84799ec4fe217d
RoMANzhula <roman1985al@ukr.net>
2025-01-05 23:50:34 +0200

    Add data_init.sql - book-store-order-service

---
+++ book-store-order-service/src/main/resources/data_init.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO products (id, name, price)
+VALUES
+    (1, 'Product 1', 100.0),
+    (2, 'Product 2', 150.0)
+;
+
+
+INSERT INTO orders (id, amount, product_id, total_cost)
+VALUES
+    (1, 2, 1, 200.0),
+    (2, 1, 2, 150.0)
+;
