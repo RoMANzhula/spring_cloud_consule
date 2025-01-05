3c6fbd5f338a0287a56065cf9f476ab03c5f6748
RoMANzhula <roman1985al@ukr.net>
2025-01-05 23:28:16 +0200

    Add pom.xml - book-store-product-service

---
+++ book-store-product-service/src/main/java/org/romanzhula/bookstoreproductservice/BookStoreProductServiceApplication.java
@@ -0,0 +1,14 @@
+package org.romanzhula.bookstoreproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BookStoreProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreProductServiceApplication.class, args);
+	}
+
+}
