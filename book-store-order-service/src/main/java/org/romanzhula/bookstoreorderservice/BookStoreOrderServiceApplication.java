34fa204bbea8edc124db9e342f747b0c5d896b4a
RoMANzhula <roman1985al@ukr.net>
2025-01-05 23:20:07 +0200

    Add ApplicationConfig - book-store-order-service

---
+++ book-store-order-service/src/main/java/org/romanzhula/bookstoreorderservice/BookStoreOrderServiceApplication.java
@@ -0,0 +1,14 @@
+package org.romanzhula.bookstoreorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BookStoreOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreOrderServiceApplication.class, args);
+	}
+
+}
