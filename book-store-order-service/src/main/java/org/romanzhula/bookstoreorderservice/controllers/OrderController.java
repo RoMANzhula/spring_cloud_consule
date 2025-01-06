ece1cf0b1c78bdac82d52b9fff03b9ac8ce5fbda
RoMANzhula <roman1985al@ukr.net>
2025-01-06 20:28:50 +0200

    Up OrderController - book-store-order-service - change to jdbc

--- book-store-order-service/src/main/java/org/romanzhula/bookstoreorderservice/controllers/OrderController.java
+++ book-store-order-service/src/main/java/org/romanzhula/bookstoreorderservice/controllers/OrderController.java
@@ -2,38 +2,54 @@ package org.romanzhula.bookstoreorderservice.controllers;
 
 import org.romanzhula.bookstoreorderservice.models.OrderInfo;
 import org.romanzhula.bookstoreorderservice.models.Product;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/order-service")
-public class OrderController {
+public class OrderController implements RowMapper<OrderInfo> {
 
     private final RestTemplate restTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-    public OrderController(RestTemplate restTemplate) {
+    public OrderController(
+            RestTemplate restTemplate,
+            JdbcTemplate jdbcTemplate
+    ) {
         this.restTemplate = restTemplate;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @GetMapping("/orders")
     public List<OrderInfo> getAllOrders() {
-        Product[] products =
+        return this.jdbcTemplate.query("SELECT * FROM order_info", this);
+    }
+
+
+    @Override
+    public OrderInfo mapRow(ResultSet rs, int rowNum) throws SQLException {
+        var productId = rs.getLong("product_id");
+
+        Product product =
                 this.restTemplate.getForObject(
-                        "http://product-service/api/product-servie/products",
-                        Product[].class
+                        "http://product-service/api/product-service/products/" + productId,
+                        Product.class
                 )
         ;
 
-        if (products == null) return Collections.emptyList();
-
-        return List.of(
-                new OrderInfo(1L, 2, products[0], (2 * products[0].price())),
-                new OrderInfo(2L, 1, products[1], products[1].price())
+        return new OrderInfo(
+                rs.getLong("id"),
+                rs.getInt("amount"),
+                product,
+                rs.getDouble("total_cost")
         );
     }
 
