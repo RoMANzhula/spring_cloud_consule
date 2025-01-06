77e94bb6148f52b9b6c245eee2ddc6bdb5f1db09
RoMANzhula <roman1985al@ukr.net>
2025-01-06 23:55:08 +0200

    Add ProductController - book-store-product-service - add db via consul

--- book-store-product-service/src/main/java/org/romanzhula/bookstoreproductservice/conrollers/ProductController.java
+++ book-store-product-service/src/main/java/org/romanzhula/bookstoreproductservice/conrollers/ProductController.java
@@ -1,23 +1,43 @@
 package org.romanzhula.bookstoreproductservice.conrollers;
 
 import org.romanzhula.bookstoreproductservice.models.Product;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
 @RestController
-@RequestMapping("/api/product-servie")
+@RequestMapping("/api/product-service")
 public class ProductController {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductController(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @GetMapping("/products")
     public List<Product> getAllProducts() {
-        return List.of(
-                new Product(1L, "First Step", 299.99),
-                new Product(2L, "Big Mouse", 159.50)
+        return this.jdbcTemplate.query("SELECT * FROM products", (rs, rowNum) ->
+                new Product(
+                        rs.getLong("id"),
+                        rs.getString("name"),
+                        rs.getDouble("price")
+                )
         );
+    }
+
+    @GetMapping("/products/{id}")
+    public Product getProducts(@PathVariable int id) {
+        String sql = "SELECT * FROM products WHERE id = ?";
+        return this.jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new Product(
+                rs.getLong("id"),
+                rs.getString("name"),
+                rs.getDouble("price")
+        ), id);
     }
 
 }
