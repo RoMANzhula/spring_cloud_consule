a56ec5f9ebe87f7e35dd1adc4856c6df7d1eb324
RoMANzhula <roman1985al@ukr.net>
2025-01-17 23:31:02 +0200

    Add DataSourceConfig - book-store-product - store the acl-token for Consul in the Vault

---
+++ book-store-product-service/src/main/java/org/romanzhula/bookstoreproductservice/configurations/DataSourceConfig.java
@@ -0,0 +1,30 @@
+package org.romanzhula.bookstoreproductservice.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @DependsOn("consulConfig") // ensures that ConsulConfig is initialized first
+    public DataSource dataSource(ConsulConfig consulConfig) {
+        // get data for DataSource from Consul
+        String dbUrl = consulConfig.getDbUrl();
+        String dbUsername = consulConfig.getDbUsername();
+        String dbPassword = consulConfig.getDbPassword();
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        dataSource.setDriverClassName("org.postgresql.Driver");
+
+        return dataSource;
+    }
+
+}
