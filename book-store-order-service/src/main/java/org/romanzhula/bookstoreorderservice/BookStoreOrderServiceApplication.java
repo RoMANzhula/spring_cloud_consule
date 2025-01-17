c3b6c2d3b107a687ac226064847d9a1e4837723a
RoMANzhula <roman1985al@ukr.net>
2025-01-18 00:14:57 +0200

    Up BookStoreOrderServiceApplication - book-store-order - store the acl-token for Consul in the Vault

--- book-store-order-service/src/main/java/org/romanzhula/bookstoreorderservice/BookStoreOrderServiceApplication.java
+++ book-store-order-service/src/main/java/org/romanzhula/bookstoreorderservice/BookStoreOrderServiceApplication.java
@@ -1,14 +1,35 @@
 package org.romanzhula.bookstoreorderservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.wavefront.WavefrontProperties;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 
 @SpringBootApplication
-public class BookStoreOrderServiceApplication {
+public class BookStoreOrderServiceApplication implements CommandLineRunner {
+
+	@Autowired
+	Environment env;
+
+	public String getAclToken() {
+		return env.getProperty("acl-token");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreOrderServiceApplication.class, args);
+	}
+
+	// check acl token from Vault for Consul
+	@Override
+	public void run(String... args) throws Exception {
+		Logger logger = LoggerFactory.getLogger(WavefrontProperties.Application.class);
+
+		logger.info("Consul ACL Token: {}", getAclToken());
 	}
 
 }
