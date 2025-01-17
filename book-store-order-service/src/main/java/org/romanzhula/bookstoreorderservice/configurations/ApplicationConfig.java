597ae631379333a8e1b28138b3317748bd6373ba
RoMANzhula <roman1985al@ukr.net>
2025-01-18 00:15:34 +0200

    Up ApplicationConfig - book-store-order - store the acl-token for Consul in the Vault

--- book-store-order-service/src/main/java/org/romanzhula/bookstoreorderservice/configurations/ApplicationConfig.java
+++ book-store-order-service/src/main/java/org/romanzhula/bookstoreorderservice/configurations/ApplicationConfig.java
@@ -1,9 +1,13 @@
 package org.romanzhula.bookstoreorderservice.configurations;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.client.RestTemplate;
+
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -13,6 +17,12 @@ public class ApplicationConfig {
     @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
     }
 
 }
